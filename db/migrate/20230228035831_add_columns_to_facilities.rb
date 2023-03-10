a4879724d746fa9cbcfba5cdea1e9ffd284c91a4
Mitchell Sweigart <mitchell.lee.sweigart@gmail.com>
2023-03-10 15:54:52 -0500

    removing a conflicting migration

--- db/migrate/20230228035831_add_columns_to_facilities.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToFacilities < ActiveRecord::Migration[7.0]
-  def change
-    add_column :facilities, :facility_id, :integer
-    add_column :facilities, :facility_type, :string
-  end
-end
