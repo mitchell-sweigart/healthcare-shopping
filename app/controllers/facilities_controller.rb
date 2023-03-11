908329a7e0e420edd97123f849042f2fc86d11a3
Mitchell Sweigart <mitchell.lee.sweigart@gmail.com>
2023-03-10 21:43:55 -0500

    distance 4.0

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -18,9 +18,9 @@ class FacilitiesController < ApplicationController
                 google_maps_direction_api = "https://maps.googleapis.com/maps/api/directions/json?origin=#{@latitude},#{@longitude}&destination=#{facility_lat},#{facility_lon}&key=AIzaSyDSL85vkykDd8e2g7Z5mzd-zJvf779k0dM"
                 direction_data_raw = URI.open(google_maps_direction_api).read
                 direction_data_hash = JSON.parse(direction_data_raw)
-                distance = direction_data_hash["routes"][0]["legs"][0]["distance"]["text"]
+                distance_to_travel = direction_data_hash["routes"][0]["legs"][0]["distance"]["text"]
 
-                facility['distance'] = distance
+                facility[:distance] = distance_to_travel
             end
         end
     end
