21d3c5bdcd49061cae37c905051edd7634161cf8
Mitchell Sweigart <mitchell.lee.sweigart@gmail.com>
2023-03-10 21:38:54 -0500

    distance 3.0

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -12,11 +12,16 @@ class FacilitiesController < ApplicationController
             @latitude = request.location.latitude
             @longitude = request.location.longitude
 
-            google_maps_direction_api = "https://maps.googleapis.com/maps/api/directions/json?origin=#{@latitude},#{@longitude}&destination=40.4612819,-79.9461137&key=AIzaSyDSL85vkykDd8e2g7Z5mzd-zJvf779k0dM"
-            direction_data_raw = URI.open(google_maps_direction_api).read
-            direction_data_hash = JSON.parse(direction_data_raw)
+            @facilities.each do |facility|
+                facility_lat = facility.latitude
+                facility_lon = facility.longitude
+                google_maps_direction_api = "https://maps.googleapis.com/maps/api/directions/json?origin=#{@latitude},#{@longitude}&destination=#{facility_lat},#{facility_lon}&key=AIzaSyDSL85vkykDd8e2g7Z5mzd-zJvf779k0dM"
+                direction_data_raw = URI.open(google_maps_direction_api).read
+                direction_data_hash = JSON.parse(direction_data_raw)
+                distance = direction_data_hash["routes"][0]["legs"][0]["distance"]["text"]
 
-            @distance = direction_data_hash["routes"][0]["legs"][0]["distance"]["text"]
+                facility['distance'] = distance
+            end
         end
     end
 
