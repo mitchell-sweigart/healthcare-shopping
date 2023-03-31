4587333721501f1822755a2ff992ec4ec75662da
Mitchell Sweigart <mitchell.lee.sweigart@gmail.com>
2023-03-30 20:38:38 -0400

    naming error fix

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -8,6 +8,6 @@ if Rails.env.development?
     Resque.redis = Redis.new(host: 'localhost', post: '6379')
 else
     uri = URI.parse(ENV['REDIS_URL'])
-    REDIS = Redis.new(host: url.host, port: url.port, password: uri.password)
+    REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
     Resque.redis = REDIS
 end
